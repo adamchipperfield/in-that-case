25c6432ec12a62e2e0f3512f414f305ea27f1ffa
Adam Chipperfield <adam.chipperfield@wemakewebsites.info>
2022-05-29 17:26:46 +0100

    BUILD: Initial build of all files with kebab case for a proof of concept

---
+++ index.js
@@ -0,0 +1,37 @@
+/**
+ * @file Entry point for the package with direct exports.
+ */
+
+/**
+ * Transforms text into standard kebab casing.
+ * @param {string} input - The user input.
+ * @returns - The transformed string.
+ */
+export function kebab(input) {
+  return input
+    .split(' ')
+
+    /**
+     * Sanitizes each word then capitalizes.
+     */
+    .map((word) => {
+      const lower = word.toLowerCase()
+
+      return lower
+        .charAt(0)
+        .toUpperCase() + lower.slice(1)
+    })
+    .join('')
+    .split('')
+    .filter((letter) => /[a-z]/i.test(letter))
+    .map((letter) => {
+      if (/[A-Z]/.test(letter)) {
+        return ` ${letter.toLowerCase()}`
+      }
+
+      return letter
+    })
+    .join('')
+    .trim()
+    .replace(/[_\s]+/g, '-')
+}
---
+++ tests/kebab.spec.js
@@ -0,0 +1,38 @@
+/**
+ * @file Tests for the kebab case helper.
+ */
+import { test, expect } from '@playwright/test'
+
+import { kebab } from '#index'
+
+test('from pascal case', () => {
+  expect(kebab('pascal case')).toEqual('pascal-case')
+})
+
+test('from title case', () => {
+  expect(kebab('Title Case')).toEqual('title-case')
+})
+
+test('from sentence case', () => {
+  expect(kebab('Sentence case')).toEqual('sentence-case')
+})
+
+test('from lower case', () => {
+  expect(kebab('lower case')).toEqual('lower-case')
+})
+
+test('from first letter lower case', () => {
+  expect(kebab('first Lower')).toEqual('first-lower')
+})
+
+test('from sentence case with random uppercase letter', () => {
+  expect(kebab('Sentence cAse')).toEqual('sentence-case')
+})
+
+test('from sentence case with special character inside a word', () => {
+  expect(kebab('Sentence ca!se')).toEqual('sentence-case')
+})
+
+test('from sentence case with special character at end', () => {
+  expect(kebab('Sentence case!')).toEqual('sentence-case')
+})
